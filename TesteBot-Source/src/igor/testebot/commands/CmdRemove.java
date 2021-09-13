64a1c1a8634979286535dd8d372368af998fe5c6
ElisaCosta025 <63672767+ElisaCosta025@users.noreply.github.com>
2021-09-13 11:59:32 -0300

    Update CmdRemove.java

--- TesteBot-Source/src/igor/testebot/commands/CmdRemove.java
+++ TesteBot-Source/src/igor/testebot/commands/CmdRemove.java
@@ -4,8 +4,6 @@ import igor.testebot.core.Command;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.ArrayList;
-import java.String.split;
-import Array.prototype.join();
 
 public class CmdRemove implements Command {
 
